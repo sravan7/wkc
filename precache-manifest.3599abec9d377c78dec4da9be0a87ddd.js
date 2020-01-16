f86eba03d806ffb3903d3452521ddcab5d3602ad
sravan7 <esd14i017@iiitdm.ac.in>
2020-01-17 04:03:17 +0530

    Updates

---
+++ precache-manifest.3599abec9d377c78dec4da9be0a87ddd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b317da1ab99630a523ee5246d7ef14",
+    "url": "https//sravan7.github.io/wkc/index.html"
+  },
+  {
+    "revision": "5b633890643d9e992b6a",
+    "url": "https//sravan7.github.io/wkc/static/css/2.821458bd.chunk.css"
+  },
+  {
+    "revision": "3ff86e195b3255b3128e",
+    "url": "https//sravan7.github.io/wkc/static/css/main.be35b07b.chunk.css"
+  },
+  {
+    "revision": "5b633890643d9e992b6a",
+    "url": "https//sravan7.github.io/wkc/static/js/2.ac1503a8.chunk.js"
+  },
+  {
+    "revision": "99c313d2c5cb9b72a03a0280cac73caf",
+    "url": "https//sravan7.github.io/wkc/static/js/2.ac1503a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ff86e195b3255b3128e",
+    "url": "https//sravan7.github.io/wkc/static/js/main.019ae2df.chunk.js"
+  },
+  {
+    "revision": "a62af1d209065ffe6578",
+    "url": "https//sravan7.github.io/wkc/static/js/runtime-main.8d942b93.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https//sravan7.github.io/wkc/static/media/logo.5d5d9eef.svg"
+  }
+]);
